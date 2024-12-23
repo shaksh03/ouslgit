8eeba5be887240704467af769957e93e5b5d53f3
shaksh03 <shanoliakshalina@gmail.com>
2024-12-23 19:24:53 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print (" hello world")
+ hello world
+>>> 
+=============== RESTART: C:/Users/shalo/OneDrive/Desktop/hello.py ==============
+Enter your name:shanoli
+your name is:shanoli
+>>> 
+=============== RESTART: C:/Users/shalo/OneDrive/Desktop/hello.py ==============
+Enter your firstname:shanoli
+Enter your lastname:akshalina
+Your name isshanoliakshalina
+>>> 
+=============== RESTART: C:/Users/shalo/OneDrive/Desktop/hello.py ==============
+Enter your firstname:shanoli
+Enter your lastname:akshalina
+Your name is shanoli akshalina
